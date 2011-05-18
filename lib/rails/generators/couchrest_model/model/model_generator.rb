e7e9e45904d6e663fc2e7a69a96e068fcaa738c7
Shenouda Bertel <ShenoudaB@gmail.com>
2011-05-19 00:39:58 +0200

    adding more options and casted model to generator

--- lib/rails/generators/couchrest_model/model/model_generator.rb
+++ lib/rails/generators/couchrest_model/model/model_generator.rb
@@ -3,13 +3,13 @@ require 'rails/generators/couchrest_model'
 module CouchrestModel
   module Generators
     class ModelGenerator < Base
-      argument :attributes, :type => :array, :default => [], :banner => "property:type property:type#, posfixing with '#' marks attribute to be indexed"
+      argument :attributes, :type => :array, :default => [], :banner => "property:type property:type"
       
       check_class_collision
       
-      class_option :timestamps, :type => :boolean, :aliases => "-T", :desc => "Add timestamps created_at and updated_at", :default => false
-      class_option :casted, :type => :boolean, :aliases => "-E", :desc => "Casted document", :default => false
       class_option :parent, :type => :string, :aliases => "-P", :desc => "Class name of parent document"
+      class_option :casted, :type => :boolean, :aliases => "-C", :desc => "Casted Model document", :default => false
+      class_option :timestamps, :type => :boolean, :aliases => "-T", :desc => "Add timestamps created_at and updated_at", :default => false
       
       def create_model_file
         template 'model.rb', File.join('app/models', class_path, "#{file_name}.rb")
