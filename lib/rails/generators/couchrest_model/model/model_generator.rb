2fa42c9ce5c3a845377cedcea66f0ec08a0193f3
Shenouda Bertel <ShenoudaB@gmail.com>
2011-05-25 17:15:01 +0200

    add module support to model generator

--- lib/rails/generators/couchrest_model/model/model_generator.rb
+++ lib/rails/generators/couchrest_model/model/model_generator.rb
@@ -15,6 +15,11 @@ module CouchrestModel
         template 'model.rb', File.join('app/models', class_path, "#{file_name}.rb")
       end
       
+      def create_module_file
+        return if regular_class_path.empty?
+        template 'module.rb', File.join('app/models', "#{class_path.join('/')}.rb") if behavior == :invoke
+      end
+      
       hook_for :test_framework
       
       protected
---
+++ lib/rails/generators/couchrest_model/model/templates/module.rb
@@ -0,0 +1,5 @@
+<% module_namespacing do -%>
+module <%= class_path.map(&:camelize).join('::') %>
+  
+end
+<% end -%>
